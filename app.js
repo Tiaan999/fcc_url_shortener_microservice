987bad6e1c8f14abbe36d447601855065dc519c9
Tiaan Willemse <tiaan1.willemse@gmail.com>
2016-02-03 05:47:11 +0000

    Still not doen with App.js

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 'use strict';
 
+var dbCount = 0;
+
 var express = require('express');
 var mongo = require('mongodb').MongoClient;
+var https = require('https');
 
 var app = express();
 require('dotenv').load();
@@ -10,74 +13,158 @@ mongo.connect(process.env.MONGO_URI, function (err, db) {
   if (err) {
     console.log('Unable to connect to the mongoDB server. Error:', err);
   } else {
-    console.log('Connection established to', process.env.MONGO_URI);
-    //db.createCollection('websites');
+    console.log('Connection established to ' + process.env.MONGO_URI + '\n');
     var websites = db.collection('websites');
 	websites.remove({});
-	console.log('All gone.');
-	var web1 = {short: 1, original: 'http://www.google.com'};
-	var web2 = {short: 2, original: 'http://www.facebook.com'};
+	//console.log('All gone.');
+	var web1 = {short: 1, original: 'https://www.google.com'};
+	var web2 = {short: 2, original: 'https://www.facebook.com'};
 	websites.insert([web1, web2], function (err, result) {
 		if (err) {
 			throw err;
 		} else {
-			console.log('First two added.\n')
+			dbCount = 2;
 			db.close();
 		}
-	})
-  }
-});
-
-mongo.connect(process.env.MONGO_URI, function (err, db) {
-  if (err) {
-    console.log('Unable to connect to the mongoDB server. Error:', err);
-  } else {
-    console.log('Connection established to', process.env.MONGO_URI);
-	var collection = db.collection('websites');
-	collection.find().toArray(function(err, items) {
-		if (err) {
-			throw err;
-		} else {
-			console.log("Here's what's in the database:");
-			console.log(items);
-			console.log('\n');
-			db.close();	
-		}
 	});
   }
 });
 
+//Redirect to homepage for instructions
+app.get('/', function (req, res) {
+	res.sendFile(process.cwd() + '/index.html');
+});
+
+//Redirect favicon.ico request
+app.get('/favicon.ico', function (req, res) {
+    res.writeHead(200, {'Content-Type': 'image/x-icon'} );
+    res.end();
+});
+
+//If something is appened to base URL, do this
 app.get('/*', function (req, res) {
 	var received = req.params[0];
-	var lastEntry = 0;
-	mongo.connect(process.env.MONGO_URI, function (err, db) {
-    	if (err) {
-    		console.log('Unable to connect to the mongoDB server. Error:', err);
-  		} else {
-    		console.log('Connection established to', process.env.MONGO_URI);
-			var collection = db.collection('websites');
-			
-			//Determine last entry in DB - Make function
-			collection.count({}, function (err, last) {
-				console.log('No problem yet.');
-					if (err) {
-						throw err;
-					} else {
-						console.log('No problem yet.');
-						lastEntry = last;
-						console.log("The last entry in the database is:" + last);
-						res.send("The last entry in the database is:" + lastEntry);
-						db.close();
-					}
-				}
-			);
-			
-		 }
+	if (received > 0 && received <= dbCount) {
+		redirectL(received, function (redirectTo) {
+			res.redirect(redirectTo);
+		})
+		console.log('You found a winner.');
+	}
+	received = received.replace('http', 'https');
+	console.log('Instruction received: ' + received);
+	
+	validLink(received, function (valid) {
+		if (valid) {
+			addToDB(received, function (position) {
+				res.send("URL Valid. Link at: " + position);
+			});
+		} else {
+			res.send("Error: URL invalid: " + received);
+			console.log("Error: URL invalid: " + received);
+		}
 	});
-	//res.send("The last entry in the database is:" + lastEntry);
 });
 
 var port = process.env.PORT;
 app.listen(port,  function () {
 	console.log('Node.js listening on port ' + port + '...');
 });
+
+function validLink(link, callback) {
+	console.log('validLink function received: ' + link);
+	var req  = https.request(link, function (res) {
+		console.log('Response: ' + res.statusCode);
+		if ((res.statusCode === 200) || (res.statusCode === 301) || (res.statusCode === 302)) {
+			console.log("URL Valid");
+			if(typeof callback == "function") {
+				callback(true);
+			}
+  		} else {
+  			console.log("URL Invalid");
+  			if(typeof callback == "function") {
+				callback(false);
+			}
+  		}
+	});
+	
+	req.end();
+	req.on('error', (e) => {
+  		console.error(e);
+	});
+}
+	
+function addToDB(link, callback) {
+	mongo.connect(process.env.MONGO_URI, function (err, db) {
+    	if (err) {
+    		console.log('Unable to connect to the mongoDB server. Error:', err);
+  		} else {
+    		console.log('Connection established to ' + process.env.MONGO_URI);
+			var collection = db.collection('websites');
+			
+			//Determine if link is in DB
+			collection.find({original: link}, {short: 1}).toArray(function (err, items) {
+				if (err) {
+					throw err;	
+				} else {
+					//console.log(items[0]);
+					if (items[0] === undefined) {
+						//Add to DB
+						dbCount++;
+						collection.insert([{short: dbCount, original: link}], function (err, result) {
+							if (err) {
+								throw err;
+							} else {
+								db.close();
+								console.log('Added to DB. Count: ' + dbCount);
+								if(typeof callback == "function") {
+									callback(dbCount);
+								}
+							}
+						});
+					} else {
+						//Print position in DB
+						db.close();
+						console.log('Link already in DB. Count: ' + items[0].short);
+						if(typeof callback == "function") {
+							callback(items[0].short);
+						}
+					}
+				}
+			});
+		 }
+	});
+}
+
+function redirectL(ref, callback) {
+	mongo.connect(process.env.MONGO_URI, function (err, db) {
+		if (err) {
+			console.log('Unable to connect to the mongoDB server. Error:', err);
+		} else {
+			console.log('Connection established to ' + process.env.MONGO_URI);
+			var collection = db.collection('websites');
+			
+			//collection.count(function (err, count) {
+			//	if (err) {
+			//		throw err;
+			//	} else {
+			//		console.log(count);
+			//	}
+			//});
+			
+			//Determine if link is in DB
+			collection.find({'short': Number(ref)}, {"_id": 0, 'original': 1}, function (err, items) {
+				if (err) {
+					throw err;	
+				} else {
+					//Print position in DB
+					console.log(items);
+					db.close();
+					
+					if(typeof callback == "function") {
+						callback(items);
+					}
+				}
+			});
+		}
+	})
+}
